efd80b1ef11419e3ae0b29047672a16019c26dff
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2020-01-08 19:50:55 -0600

    user can no longer enter a negative value

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -6,7 +6,12 @@ public class Area {
     public static void main(String[] args) {
         Scanner radius = new Scanner (System.in);
         System.out.println("What is the radius of the circle?");
-        double userRadius = radius.nextInt();
+        double userRadius = radius.nextDouble();
+
+        if(userRadius < 0){
+            System.out.println("Only numeric values can be entered!");
+            System.exit(0);
+        }
 
         double area = Circle.getArea(userRadius);
         System.out.println("The area of the circle is " + area);
