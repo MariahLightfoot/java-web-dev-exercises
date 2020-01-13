a4bc61ad662360a9ac7affa556d8ea7f60ee8880
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2020-01-13 16:58:21 -0600

    created an array of values and a loop to print each value

--- src/org/launchcode/java/exercises/control_flow_and_collections/Array.java
+++ src/org/launchcode/java/exercises/control_flow_and_collections/Array.java
@@ -1,4 +1,25 @@
 package org.launchcode.java.exercises.control_flow_and_collections;
 
 public class Array {
+    public static void main(String[] args) {
+        //declares array and allocates 7 spaces for memory
+        int[] values = new int[6];
+
+        //populating array - has to be a better way
+        values[0] = 1;
+        values[1] = 1;
+        values[2] = 2;
+        values[3] = 3;
+        values[4] = 5;
+        values[5] = 8;
+
+        //looping through values array
+        for (int i=0; i<6; i++){
+            System.out.println(values[i]);
+        }
+
+        //why doesn't values print like this?
+        //System.out.println(values);
+
+    }
 }
