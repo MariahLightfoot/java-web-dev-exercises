d285a78a57f6348dc3d385be2d71a756a2786b53
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2020-01-13 19:49:39 -0600

    converted the string from the 3.7.1 into an ArrayList

--- src/org/launchcode/java/exercises/control_flow_and_collections/ArrayListPractice.java
+++ src/org/launchcode/java/exercises/control_flow_and_collections/ArrayListPractice.java
@@ -1,6 +1,7 @@
 package org.launchcode.java.exercises.control_flow_and_collections;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayListPractice {
@@ -8,21 +9,25 @@ public class ArrayListPractice {
 
         //first part of 3.7.2 ArrayList Practice
         //created an ArrayList of integers
-        ArrayList<Integer> integers = new ArrayList<>(10);
+        ArrayList<Integer> numbers = new ArrayList<>();
 
-        //adding elements to integers array
+        //adding elements to numbers array
         int sum = 0;
 
         for(int i=1; i<11; i++){
-            integers.add(i);
+            numbers.add(i);
 
             //only adding i to sum if it is even
             if(i % 2 == 0){
                 sum += i;
             }
-
         }
 
         System.out.println(sum);
+
+        //second part of 3/7/2 ArrayList Practice
+        String phrase = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+        ArrayList<String> letters = new ArrayList<String>(Arrays.asList(phrase.split(" ")));
+        System.out.println(letters);
     }
 }
