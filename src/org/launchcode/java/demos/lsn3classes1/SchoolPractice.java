c02f96e11d92534029cd3f014544226d875df371
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2020-01-20 09:35:51 -0600

    crafted a print out in the School class

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -5,9 +5,32 @@ import java.util.HashMap;
 public class SchoolPractice {
     public static void main(String[] args) {
 
-        Student firstStudent = new Student ("Mariah", 1, 1, 4.0);
-        //Course firstCourse = new Course ("Science", 1, firstStudent.getName(), firstStudent.getStudentId());
+        //created an instance of Student to be used in the Course class
+        Student firstStudent = new Student ("Mariah Lightfoot", 1, 1, 4.0);
+        System.out.println("Student Information: " + "\n" +
+                "Name: " + firstStudent.getName() + "\n" +
+                "ID: " + firstStudent.getStudentId() + "\n" +
+                "Credit(s): " + firstStudent.getNumberOfCredits() + "\n" +
+                "GPA: " + firstStudent.getGpa()+ "\n");
 
+        //created an instance of the HashMap used in Course class and populated it using the Student class
+        HashMap <String, Integer> firstStudentNameAndId = new HashMap<>();
+        firstStudentNameAndId.put(firstStudent.getName(), firstStudent.getStudentId());
+
+        //created an instance of the Course class
+        //stored the course name in a new variable to be used by the Teacher class
+        Course firstCourse = new Course ("Calculus", 1, firstStudentNameAndId);
+        String firstTeacherSubject = firstCourse.getCourseName();
+        System.out.println("Course Information: " + "\n" +
+                "Name: " + firstCourse.getCourseName() + "\n" +
+                "ID: " + firstCourse.getCourseId() + "\n" +
+                "Students Enrolled: " + firstStudentNameAndId + "\n");
+
+        Teacher firstTeacher = new Teacher ("Ethan", "Lightfoot", firstTeacherSubject, 3);
+        System.out.println("Teacher Information: " + "\n" +
+                "Name: " + firstTeacher.getFirstName()+ " " + firstTeacher.getLastName() + "\n" +
+                "Courses: " + firstTeacherSubject + "\n" +
+                "Experience: " + firstTeacher.getYearsTeaching());
     }
 }
 
